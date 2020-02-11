f165d026ba3f2e9f760e17fbd95b2bac7d9f7ba2
lukejdav <57575306+lukejdav@users.noreply.github.com>
2020-02-11 18:26:07 +0000

    Update generate.sh
    Now has DejaVuSans-Bold as default font, as this works well with alchemy

--- generate.sh
+++ generate.sh
@@ -3,12 +3,13 @@
 echo "Running for" $1 $2
 
 FONTSIZE=35
+FONTFILE=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf
 ffmpeg -f lavfi -i color=c=#000000@0:s=1920x1080,format=rgba -vf \
-"drawtext=text=$1:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2-744:y=(h-text_h)/2-468-$FONTSIZE/2-5:, \
-drawtext=text=$1:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2-312:y=(h-text_h)/2-468-$FONTSIZE/2-5:, \
-drawtext=text=$1:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2+156:y=(h-text_h)/2-468-$FONTSIZE/2-5:, \
-drawtext=text=$1:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2+660:y=(h-text_h)/2-468-$FONTSIZE/2-5:, \
-drawtext=text=$2:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2-744:y=(h-text_h)/2-468+$FONTSIZE/2+5:, \
-drawtext=text=$2:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2-312:y=(h-text_h)/2-468+$FONTSIZE/2+5:, \
-drawtext=text=$2:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2+156:y=(h-text_h)/2-468+$FONTSIZE/2+5:, \
-drawtext=text=$2:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2+660:y=(h-text_h)/2-468+$FONTSIZE/2+5:" public/output.png -y
+"drawtext=text=$1:$FONTFILE:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2-744:y=(h-text_h)/2-468-$FONTSIZE/2-5:, \
+drawtext=text=$1:$FONTFILE:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2-312:y=(h-text_h)/2-468-$FONTSIZE/2-5:, \
+drawtext=text=$1:$FONTFILE:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2+156:y=(h-text_h)/2-468-$FONTSIZE/2-5:, \
+drawtext=text=$1:$FONTFILE:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2+660:y=(h-text_h)/2-468-$FONTSIZE/2-5:, \
+drawtext=text=$2:$FONTFILE:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2-744:y=(h-text_h)/2-468+$FONTSIZE/2+5:, \
+drawtext=text=$2:$FONTFILE:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2-312:y=(h-text_h)/2-468+$FONTSIZE/2+5:, \
+drawtext=text=$2:$FONTFILE:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2+156:y=(h-text_h)/2-468+$FONTSIZE/2+5:, \
+drawtext=text=$2:$FONTFILE:fontcolor=white:fontsize=$FONTSIZE:x=(w-text_w)/2+660:y=(h-text_h)/2-468+$FONTSIZE/2+5:" output.png -y
